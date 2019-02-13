8796b22ac615cde6a95d16159d469c68a39e77d5
Marco Laspe <marco@rockiger.com>
2019-02-13 11:48:46 +0100

    Statusbar

--- src/app.jsx
+++ src/app.jsx
@@ -10,12 +10,38 @@ import {
   Popover,
   Menu,
   MenuItem,
+  Colors,
 } from '@blueprintjs/core';
 
 const showMessageBox = remote.dialog.showMessageBox;
 const app = remote.app;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      statistic: {
+        row: 0,
+        col: 0,
+        chars: 0,
+      },
+    };
+    this.onInput = this.onInput.bind(this);
+  }
+
+  onInput() {
+    const textField = document.querySelector('#TextField');
+    const textBeforCaret = textField.value.slice(0, textField.selectionStart);
+    const rows = textBeforCaret.split('\n');
+    const row = rows.length;
+    const col = rows.pop().length;
+    this.setState({ statistic: {
+      row,
+      col,
+      chars: textField.value.length,
+    } });
+  }
+
   render() {
     return (
       <div id="Layout" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
@@ -65,6 +91,17 @@ export default class App extends React.Component {
           }}
           onKeyDown={this.onInput}
         />
+        <div
+          id="StatusBar"
+          style={{
+            backgroundColor: Colors.LIGHT_GRAY5,
+            borderTop: `1px solid ${Colors.LIGHT_GRAY1}`,
+            height: '50px',
+            padding: 12,
+          }}
+        >
+          Cursor at row {this.state.statistic.row} column {this.state.statistic.col} - {this.state.statistic.chars} chars in document
+        </div>
       </div>);
   }
 }
